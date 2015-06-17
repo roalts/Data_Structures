8e468fc0faac693da74b3e9037c3247b8de2b408
Raghav Apoorv <raghav.apoorv@gmail.com>
2015-06-18 00:07:45 +0530

    print n from the end, linked list

--- Data Structures/src/linked_lists/PrintNthFromTheEnd.java
+++ Data Structures/src/linked_lists/PrintNthFromTheEnd.java
@@ -2,4 +2,35 @@ package linked_lists;
 
 public class PrintNthFromTheEnd {
 
+	public static void printFromEnd(Node<Integer> head, int n) {
+		int totalNodes = numberofNodes(head);
+		int i = 0;
+		Node<Integer> temp = head;
+		while (temp != null) {
+			if (i == totalNodes - n ) {
+				System.out.println(temp.data);
+				return;
+			}
+			temp = temp.next;
+			i++;
+		}
+	}
+
+	public static int numberofNodes(Node<Integer> head) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		Node<Integer> temp = head;
+		while (temp != null) {
+			temp = temp.next;
+			count++;
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+
+		Node<Integer> head = LinkedListUse.takeInput();
+		printFromEnd(head, 2);
+
+	}
 }
