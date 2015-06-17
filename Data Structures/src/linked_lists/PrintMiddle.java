91185684b4b7a2b6ec4289cfcc084afa043f2c88
Raghav Apoorv <raghav.apoorv@gmail.com>
2015-06-17 22:15:21 +0530

    operations on linked lists

---
+++ Data Structures/src/linked_lists/PrintMiddle.java
@@ -0,0 +1,27 @@
+package linked_lists;
+
+import javax.media.j3d.Link;
+
+public class PrintMiddle {
+
+	public static int printMiddleNode(Node<Integer> head) {
+		Node<Integer> slowPointer = head;
+		Node<Integer> fastPointer = head;
+		if(head != null) {
+			while(fastPointer != null && fastPointer.next != null){
+				slowPointer = slowPointer.next;
+				fastPointer = fastPointer.next.next;
+			}
+			return slowPointer.data;
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Node<Integer> head = LinkedListUse.takeInput();
+		LinkedListUse.printLL(head);
+		System.out.println();
+		System.out.println(printMiddleNode(head));
+	}
+
+}
