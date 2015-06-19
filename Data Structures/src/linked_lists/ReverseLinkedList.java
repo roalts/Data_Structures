5b15c4e4759232c9761082568bfddee900ca6f02
Raghav Apoorv <raghav.apoorv@gmail.com>
2015-06-19 19:34:06 +0530

    linkedlist reversed

--- Data Structures/src/linked_lists/ReverseLinkedList.java
+++ Data Structures/src/linked_lists/ReverseLinkedList.java
@@ -2,9 +2,22 @@ package linked_lists;
 
 public class ReverseLinkedList {
 
+	public static Node<Integer> reverseLinkedList(Node<Integer> head){
+		Node<Integer> tail = head;
+		Node<Integer> newHead = head;
+		while(head.next != null){
+			tail = head.next;
+			head.next = tail.next;
+			tail.next = newHead;
+			newHead = tail;
+		}
+		return newHead;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Node<Integer> head = LinkedListUse.takeInput();
+		head = reverseLinkedList(head);
+		LinkedListUse.printLL(head);
 	}
 
 }
