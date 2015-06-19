be0ceec51e0e3ee6d0e3e39ef3ad85d7b2f308fc
Raghav Apoorv <raghav.apoorv@gmail.com>
2015-06-19 20:00:02 +0530

    recursive reversing linkedlist

--- Data Structures/src/linked_lists/ReverseLinkedList.java
+++ Data Structures/src/linked_lists/ReverseLinkedList.java
@@ -13,10 +13,24 @@ public class ReverseLinkedList {
 		}
 		return newHead;
 	}
+	public static Node<Integer> reverseUsingRecursion(Node<Integer> head){
+		if(head == null || head.next == null){
+			return head;
+		}
+		
+		Node<Integer> smallerHead = reverseLinkedList(head.next);
+		Node<Integer> tail = smallerHead;
+		while(tail.next != null){
+			tail = tail.next;
+		}
+		tail.next = head;
+		head.next = null;
+		return smallerHead;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Node<Integer> head = LinkedListUse.takeInput();
-		head = reverseLinkedList(head);
+		head = reverseUsingRecursion(head);
 		LinkedListUse.printLL(head);
 	}
 
