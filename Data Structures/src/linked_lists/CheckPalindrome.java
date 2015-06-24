c44406f6af8e7ef3f9411aaa616a57fc35e53a07
Raghav Apoorv <raghav.apoorv@gmail.com>
2015-06-24 20:54:25 +0530

    palindrome using middle element

--- Data Structures/src/linked_lists/CheckPalindrome.java
+++ Data Structures/src/linked_lists/CheckPalindrome.java
@@ -2,4 +2,52 @@ package linked_lists;
 
 public class CheckPalindrome {
 
+	public static Node<Integer> returnMiddle(Node<Integer> head) {
+		Node<Integer> slowPointer = head;
+		Node<Integer> fastPointer = head;
+		if(head != null){
+			while(fastPointer != null && fastPointer.next != null) {
+				fastPointer = fastPointer.next.next;
+				slowPointer = slowPointer.next;
+			}
+			return slowPointer;
+		}
+		return null;
+	}
+	
+	public static Node<Integer> reverseLL(Node<Integer> head) {
+		if(head == null || head.next == null){
+			return head;
+		}
+		
+		Node<Integer> smallerHead = reverseLL(head.next);
+		
+		head.next.next = head;
+		head.next = null;
+		return smallerHead;
+	}
+	public static boolean checkPalindrome(Node<Integer> head) {
+		Node<Integer> mid = head;
+		if(head != null) {
+			mid = returnMiddle(head);
+			mid = reverseLL(mid);
+		}
+		while(head != null && mid != null) {
+			if(head.data != mid.data)
+				return false;
+			else {
+				mid = mid.next;
+				head = head.next;
+			}
+				
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		Node<Integer> head = LinkedListUse.takeInput();
+		System.out.println(checkPalindrome(head));
+		
+	}
 }
+ 
