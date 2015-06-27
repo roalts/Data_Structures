29042784491a48b49565e24dd49cda2de617c0c7
Raghav Apoorv <raghav.apoorv@gmail.com>
2015-06-27 13:03:38 +0530

    insertion in a sorted manner, linked list

--- Data Structures/src/linked_lists/InsertionSort.java
+++ Data Structures/src/linked_lists/InsertionSort.java
@@ -1,5 +1,37 @@
 package linked_lists;
 
+import java.util.Scanner;
+
 public class InsertionSort {
 
+	public static Node<Integer> insert(Node<Integer> head, Node<Integer> newNode){
+		Node<Integer> tail = head;
+		if(head == null){
+			return newNode;
+		}
+		else if(head.data > newNode.data) {
+			newNode.next = head;
+			head = newNode;
+		}
+		else {
+			while(tail.next != null) {
+				if(newNode.data < tail.next.data) {
+					Node<Integer> temp = tail.next;
+					tail.next = newNode;
+					newNode.next = temp;
+					return head;
+				}
+				tail = tail.next;
+			}
+		}
+		return head;
+	}
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		Node<Integer> head = LinkedListUse.takeInput();
+		Node<Integer> newNode = new Node<Integer>();
+		newNode.data = s.nextInt();
+		head = insert(head, newNode);
+		LinkedListUse.printLL(head);
+	}
 }
