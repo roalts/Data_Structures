ca4e9f297cb345a1865b1c283d2639d4873f6afa
Raghav Apoorv <raghav.apoorv@gmail.com>
2015-08-23 18:11:02 +0530

    unique char

--- Data Structures/src/arrays_and_strings/UniqueCharacters.java
+++ Data Structures/src/arrays_and_strings/UniqueCharacters.java
@@ -1,51 +1,38 @@
 package arrays_and_strings;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
+
 
 
 public class UniqueCharacters {
 
-
-	public static boolean uniqueCharAlgo(String s) {
-		for(int i = 0; i < s.length(); i++) {
-			for(int j = 0; j < s.length(); j++) {
-				if(i != j) {
-					if(s.charAt(i) == s.charAt(j)) {
-						return false;
-					}
-				}
-			}
-		}
-		return true;
-	}
-	
-	public static boolean isUniqueChar(String input) {
-		int count[] = new int[256];
+	public static boolean uniqueCharacters(String input) {
+		int[] count = new int[256];
 		for(int i = 0; i < input.length(); i++) {
-			count[input.charAt(i)]++; 
+			count[input.charAt(i)]++;
 		}
 		for(int i = 0; i < input.length(); i++) {
-			if(count[input.charAt(i)] != 1)
+			if(count[input.charAt(i)] > 1)
 				return false;
 		}
 		return true;
+		
 	}
 	
-	public static boolean isUniqueHashMap(String input) {
-		HashMap<Integer, Character> map = new HashMap<Integer, Character>();
+	public static boolean isUnique(String input) {
+		HashSet<Character> set = new HashSet<Character>();
 		for(int i = 0; i < input.length(); i++) {
-			if(map.containsValue(input.charAt(i))) {
-				map.put(key, value)
-			}
+			if(set.contains(input.charAt(i)))
+				return false;
+			else
+				set.add(input.charAt(i));
 		}
+		return true;
 	}
-	
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String input = s.nextLine();
-		System.out.println(isUniqueChar(input));
+		System.out.println(isUnique(input));
 	}
 
 }
